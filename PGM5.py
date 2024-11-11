570bbefcdce8ff9656da4e10481d20f1d5fdd1d2
Anggelo <anstalyn1999@gmail.com>
2024-11-10 21:47:17 -0500

    feat: :heavy_plus_sign: Number counter

---
+++ PGM5.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template_string
+import webbrowser
+from threading import Timer
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    html = '''
+    <!DOCTYPE html>
+    <html>
+        <head><title>Click Counter</title></head>
+        <body>
+            <h1>Counter: <span id="counter">0</span></h1>
+            <button onclick="incrementCounter()">Increment</button>
+            <script>
+                let count = 0;
+                function incrementCounter() {
+                    count++;
+                    document.getElementById('counter').textContent = count;
+                }
+            </script>
+        </body>
+    </html>
+    '''
+    return render_template_string(html)
+
+def open_browser():
+    webbrowser.open('http://127.0.0.1:8080')
+
+if __name__ == '__main__':
+    from waitress import serve
+    Timer(1.5, open_browser).start()
+    serve(app, host="127.0.0.1", port=8080)
