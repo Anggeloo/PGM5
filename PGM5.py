0880e46fbf2af6be2e6c6892bdf96e2a1d8a41ba
Anggelo <anstalyn1999@gmail.com>
2024-11-11 08:17:04 -0500

    feat: :heavy_plus_sign: Add DockerFile

--- PGM5.py
+++ PGM5.py
@@ -31,4 +31,4 @@ def open_browser():
 if __name__ == '__main__':
     from waitress import serve
     Timer(1.5, open_browser).start()
-    serve(app, host="127.0.0.1", port=8080)
+    serve(app, host="0.0.0.0", port=8080)
